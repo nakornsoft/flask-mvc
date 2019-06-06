ee397c6a633e29b911eda3f8e6b116bfa71f1de2
Nakhonsoft <nakhonsoft@gmail.com>
2019-06-06 17:18:37 +0700

    Update __init__.py

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 __version__ = '0.1'
-from flask import Flask
+from flask import Flask, url_for, render_template, request, Response
 from flask_sqlalchemy import SQLAlchemy 
 from flask_marshmallow import Marshmallow
 
 
-app = Flask(__name__)
-app = Flask(__name__,template_folder='views')
+app = Flask(__name__, static_folder="static", template_folder="views")
 app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
